0ea740fb34482d22123ac7f9b853819c3152abb3
GuidoOffermans <guidooffermans@hotmail.com>
2019-08-03 20:04:35 +0200

    first commit

--- README.md
+++ README.md
@@ -69,3 +69,4 @@ This section has moved here: https://facebook.github.io/create-react-app/docs/tr
 # GuidoOffermans-C
 # GuidoOffermans-C
 # GuidoOffermans-C
+# GuidoOffermans-C
---
+++ build/precache-manifest.8e5ae575299b59f1b223e8f406f4a771.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "542fab714d8a55440b60950c9ba58c05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdb09de65fa0fc1f7402",
+    "url": "/static/css/main.ffc0df69.chunk.css"
+  },
+  {
+    "revision": "ec17dd9d751765d291bb",
+    "url": "/static/js/2.609907aa.chunk.js"
+  },
+  {
+    "revision": "cdb09de65fa0fc1f7402",
+    "url": "/static/js/main.b900a6af.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "13191139e9b6fb74ac3cdc04b7085fff",
+    "url": "/static/media/Banner.13191139.png"
+  },
+  {
+    "revision": "90262f921d8e4fcf715c9b8f91830898",
+    "url": "/static/media/behance.90262f92.svg"
+  },
+  {
+    "revision": "ab51426f6ba1af829d4a5cd17145914a",
+    "url": "/static/media/galaxybackground.ab51426f.jpg"
+  },
+  {
+    "revision": "1349538f462f23b6bb30acdf38af2dee",
+    "url": "/static/media/github.1349538f.svg"
+  },
+  {
+    "revision": "80876ac613d78ab5eee8d65aa7392213",
+    "url": "/static/media/guido-profile.80876ac6.png"
+  },
+  {
+    "revision": "29751e48c3f1cc54247c1a1c2ccfe6ed",
+    "url": "/static/media/linkedin.29751e48.svg"
+  }
+]);
